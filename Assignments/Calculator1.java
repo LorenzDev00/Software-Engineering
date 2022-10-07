07a6e2532a9f1c35655a327e3009c93f709148ac
Lorenzo Bignami <95931734+LorenzDev00@users.noreply.github.com>
2022-10-07 22:13:53 +0200

    Checkers add

--- Assignments/Calculator1.java
+++ Assignments/Calculator1.java
@@ -4,62 +4,84 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("- - - Inserire numeri - - -");
+		System.out.println("- - - Digit the numbers  - - -");
 		
-		//Scanner primo numero
-		double userIn1 = 0;
-		try {
-			Scanner firstNum = new Scanner(System.in);
-			System.out.println("Inserire il primo numero: ");
-			userIn1 = firstNum.nextDouble(); 
+		//Scanner first number
+		double userIn1 = 0; 
+		while(true) {
+			try {
+				Scanner firstNum = new Scanner(System.in);
+				System.out.println("Digit first number: ");
+				userIn1 = firstNum.nextDouble(); 
+				break;
+			}
+			catch(Exception e) {
+				System.out.println("Unknown number format: " + e);
+				continue; 
+
+			}
 		}
-		catch(Exception e) {
-			System.out.println("Numero non riconosciuto: " + e);
-		}
-		
-		//Scanner secondo numero
+
+				
+		//Scanner second number
 		double userIn2 = 0; 
-		try {
-			Scanner secondNum = new Scanner(System.in);
-			System.out.println("Inserire il secondo numero: ");
-			userIn2 = secondNum.nextDouble(); 
+		while(true) {
+			try {
+				Scanner secondNum = new Scanner(System.in);
+				System.out.println("Digit second number: ");
+				userIn2 = secondNum.nextDouble(); 
+				break;
+			}
+			catch(Exception e) {
+				System.out.println("Unknown number format: " + e);
+				continue; 
+			}
 		}
-		catch(Exception e) {
-			System.out.println("Numero non riconosciuto: " + e);
-		}
-		
-		//Scanner operatore
-		Scanner operator = new Scanner(System.in);
-		System.out.println("Scegliere operazione:\n [+] Somma \n [-] Sottrazione \n [/] Divisione \n [*] Prodotto \n [%] Divisione");
-		String op = operator.nextLine(); 
-		
-		//Elaboro operatore per decidere operazione matematica 
-		System.out.println("Dati inseriti:" + userIn1 + " " + userIn2 + " " + op);
-		switch(op) {
-		case "+":
-			double result1 = userIn1 + userIn2; 
-			System.out.println(result1);
-			break;
-		case "-":
-			double result2 = userIn1 - userIn2; 
-			System.out.println(result2);
-			break;
-		case "/":
-			double result3 = userIn1 / userIn2; 
-			System.out.println(result3);
-			break;
-		case "*":
-			double result4 = userIn1 * userIn2; 
-			System.out.println(result4);
-			break;
-		case "%":
-			double result5 = userIn1 % userIn2; 
-			System.out.println(result5);
-			break;
-		default:
-			System.out.println("Operazione non riconosciuta");
+		//Scanner operator
+		boolean isTrue = true;
+		while(isTrue) {
+			Scanner operator = new Scanner(System.in);
+			System.out.println("Select:\n [+] Sum \n [-] Subtraction \n [/] Integer Division \n [*] Multiplication \n [%] Division");
+			String op = operator.nextLine(); 
 			
+			//Applying the operation to the numbers 
+			System.out.println("Inserted data: " + userIn1 + " " + op + " " + userIn2);
+			System.out.println("Result: ");
+		
+			switch(op) {
+			case "+":
+				double result1 = userIn1 + userIn2; 
+				System.out.println(result1);
+				isTrue = false;
+				break;
+			case "-":
+				double result2 = userIn1 - userIn2; 
+				System.out.println(result2);
+				isTrue = false;
+				break;
+			case "/":
+				double result3 = userIn1 / userIn2; 
+				System.out.println(result3);
+				isTrue = false;
+				break;
+			case "*":
+				double result4 = userIn1 * userIn2; 
+				System.out.println(result4);
+				isTrue = false;
+				break;
+			case "%":
+				double result5 = userIn1 % userIn2; 
+				System.out.println(result5);
+				isTrue = false;
+				break;
+			default:
+				System.out.println("Unknown operation, digit the operator again");
+				
+				
+		 }
+		 				
 		}
 		
 	}
 }
+
